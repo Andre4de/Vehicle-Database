72f785f62b09f82e0b7b3d27a70e5eb0fc612f6b
psyaf5 <psyaf5@exmail.nottingham.ac.uk>
2024-04-10 12:27:02 +0100

    Updating content

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,47 @@
-console.log("I hope this works.")
+document.addEventListener("DOMContentLoaded", function() {
+
+    console.log("DOM fully loaded and parsed");
+    // Your code here
+
+document.getElementById('vehicleSrchForm').addEventListener('submit', (event2)=>{
+    event2.preventDefault();
+    displayThis("vehSearch","vehicleMain");
+})
+
+
+document.getElementById('pplLicenseForm').addEventListener('submit', (event1)=>{
+    event1.preventDefault();
+    displayThis("pplLicSearch","searchResults");
+})
+
+
+
+
+
+
+
+
+
+
+
+function displayThis(searchInput, searchResultsDiv)
+{
+    let searchIn = document.getElementById(searchInput).value;
+
+    let divArea = document.getElementById(searchResultsDiv);
+
+    divArea.innerHTML = '';
+
+    let searchResultParagraph = document.createElement('p');
+
+    searchResultParagraph.textContent = searchIn;
+
+    divArea.appendChild(searchResultParagraph);
+}
+
+});
+
+
+
+
+
